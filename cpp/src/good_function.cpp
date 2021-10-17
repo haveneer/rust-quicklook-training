8591d515d9cd25ee4a3b8dd184a9b449450e1382
Pascal Havé <hpwxf@haveneer.com>
2021-10-17 16:24:27 +0200

    Fix "good function" for C++

--- cpp/src/good_function.cpp
+++ cpp/src/good_function.cpp
@@ -1,12 +1,16 @@
+#include <string>
+
 struct Object {
-  [[nodiscard]] constexpr int method() const noexcept { return 1; }
+  explicit Object(std::string s) noexcept : attr(std::move(s)) {}
+  [[nodiscard]] constexpr auto method() const noexcept { return 0; }
+  const std::string attr;
 };
 
-[[nodiscard]] constexpr int function(const Object &o) noexcept(&Object::method) {
+[[nodiscard]] constexpr auto function(const Object &o) noexcept(noexcept(&Object::method)) {
   return o.method();
 }
 
 int main() {
-  Object o;
+  Object o("String");
   return function(o);
 }
