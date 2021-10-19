56b264a2099b422f22bf1b4310cce2180134c516
Pascal Havé <hpwxf@haveneer.com>
2021-10-19 13:46:36 +0200

    Improve expression example

--- rs/tests/expression.rs
+++ rs/tests/expression.rs
@@ -20,6 +20,20 @@ fn main() {
         println!("type of {{ 1; }} is {}", x.type_name());
         assert_eq!(x.type_name(), "()");
     }
+
+    { // without semicolon
+        let test = true;
+        let x = if test { "Hello" } else { "World" };
+        println!("type of x is {}", x.type_name());
+        assert_eq!(x.type_name(), "&str");
+    }
+
+    { // with semicolon
+        let test = true;
+        let y = if test { "Hello"; } else { "World"; };
+        println!("type of y is {}", y.type_name());
+        assert_eq!(y.type_name(), "()");
+    }
 }
 
 #[test]
