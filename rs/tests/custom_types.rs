f2b63619a69a70652e7490b083ba6806760e4b8a
Pascal Havé <hpwxf@haveneer.com>
2022-01-05 19:12:25 +0100

    Reformat code

--- rs/tests/custom_types.rs
+++ rs/tests/custom_types.rs
@@ -1,18 +1,18 @@
 mod tests {
     use chrono::prelude::*;
 
-    struct Person(&'static str);
-
-    struct CardType(&'static str);
-
     #[test]
     fn custom_types() {
+        struct Person(&'static str);
+
         struct Birthday {
             person: Person,
             date: Date<Utc>,
         }
 
         let _epoch = Birthday { person: Person("Unix Time"), date: Utc.ymd(1970, 1, 1) };
+
+        struct CardType(&'static str);
 
         enum PaymentMethod {
             Cash,
