cb1c96ff65a9ae2a9ffeb2cafc5aeddc3b914fe6
Pascal Havé <hpwxf@haveneer.com>
2022-07-02 21:43:23 +0200

    Remove #feature since format_args_capture is now stable

--- rs/tests/format_WIP.rs
+++ rs/tests/format_WIP.rs
@@ -1,4 +1,5 @@
-#![feature(format_args_capture)] // RFC2795 : https://rust-lang.github.io/rfcs/2795-format-args-implicit-identifiers.html
+// #![feature(format_args_capture)] // RFC2795 : https://rust-lang.github.io/rfcs/2795-format-args-implicit-identifiers.html
+// the feature `format_args_capture` has been stable since 1.58.0 and no longer requires an attribute to enable
 
 #[cfg(test)]
 mod tests {
