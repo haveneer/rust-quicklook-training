a737a5d858a40644b8c543245715104f59a22600
Pascal Havé <hpwxf@haveneer.com>
2023-04-14 13:54:35 +0200

    Improve custom type example

--- rs/tests/custom_types.rs
+++ rs/tests/custom_types.rs
@@ -3,26 +3,30 @@ mod tests {
 
     #[test]
     fn custom_types() {
-        struct Person(&'static str);
+        type ComposedName = (String, String); // anonymous tuple to define a new type
 
+        struct Person(ComposedName); // named tuple
+
+        // struct with named fields
         struct Birthday {
             person: Person,
             date: Date<Utc>,
         }
 
+        // struct instanciation
         let _epoch = Birthday {
-            person: Person("Unix Time"),
+            person: Person(("Unix".into(), "Time".into())), // reuse type definition
             date: Utc.ymd(1970, 1, 1),
         };
 
-        struct CardType(&'static str);
+        struct CardType; // unit struct
 
         enum PaymentMethod {
-            Cash,
-            Cheque { number: u32 },
-            Card(CardType),
+            Cash,                   // case without variation
+            Cheque { number: u32 }, // inlined struct case definition
+            Card(CardType, u64),    // tuple case definition
         }
 
-        let _payment = PaymentMethod::Card(CardType("Visa"));
+        let _payment = PaymentMethod::Card(CardType, 42);
     }
 }
