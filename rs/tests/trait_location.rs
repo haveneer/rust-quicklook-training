71f9b065b25dfc106c22d0bc65e5f985dd62579b
Pascal Havé <hpwxf@haveneer.com>
2023-04-15 18:44:59 +0200

    Additional example to place trait implementation

---
+++ rs/tests/trait_location.rs
@@ -0,0 +1,29 @@
+mod my_trait {
+    pub trait MyTrait {
+        fn f();
+    }
+}
+
+mod my_struct {
+    pub struct MyStruct;
+}
+
+mod my_impl {
+    impl super::my_trait::MyTrait for super::my_struct::MyStruct {
+        fn f() {}
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        // help: items from traits can only be used if the trait is in scope
+        // help: the following trait is implemented but not in scope
+        use my_trait::MyTrait as _; // if it can cause symbol collision
+        //                          // you can mask it with an alias
+
+        my_struct::MyStruct::f();
+    }
+}
