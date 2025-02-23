eacd9ae17c033e0c85207827fab57e1e4f8e6e26
Pascal Havé <hpwxf@haveneer.com>
2025-02-23 22:53:55 +0100

    Add extra syntax example for turbofish

--- rs/tests/curious_turbofish.rs
+++ rs/tests/curious_turbofish.rs
@@ -1,3 +1,15 @@
+trait MyTrait {
+    fn new() -> Self;
+}
+
+impl<T> MyTrait for Vec<T> {
+    fn new() -> Self {
+        println!("Vec::new from MyTrait");
+        Vec::<T>::new()
+    }
+}
+
+
 #[test]
 fn test() {
     let v = vec![1];
@@ -9,6 +21,9 @@ fn test() {
     x.push(1);
 
     let mut x = <Vec<i32>>::new(); // no turbo fish but outer < >
+    x.push(1);
+
+    let mut x = <Vec<i32> as MyTrait>::new(); // explicit ambiguous call
     x.push(1);
 
     let x = <String>::new(); // <T>::method syntax (sometimes useful to remove ambiguities)
