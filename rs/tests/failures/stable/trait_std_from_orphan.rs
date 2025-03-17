b35676ff2a6a1cf9d7fbbbc31d41018018070cf1
Pascal Havé <hpwxf@haveneer.com>
2025-03-17 16:08:27 +0100

    Fix typo in method signature

--- rs/tests/failures/stable/trait_std_from_orphan.rs
+++ rs/tests/failures/stable/trait_std_from_orphan.rs
@@ -13,7 +13,7 @@ impl From<MyType> for Vec<i32> {
 }
 
 impl From<String> for Vec<i32> {
-    fn from(_value: MyType) -> Self {
+    fn from(_value: String) -> Self {
         todo!() // orphan rule: invalid car tout est non local
     }
 }
